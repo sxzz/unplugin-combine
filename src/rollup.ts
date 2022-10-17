69948b9df1cd049cb6f9b92c4c4003c5641d325d
三咲智子 <sxzz@sxzz.moe>
2022-10-17 21:09:44 +0800

    refactor: support rollup 3

--- src/rollup.ts
+++ src/rollup.ts
@@ -1,45 +1,11 @@
 import { resolvePlugin } from '.'
-import type {
-  CombineOptions,
-  Factory,
-  FactoryOutput,
-  RollupPlugin,
-  RollupPluginList,
-  VitePlugin,
-} from './types'
-
-export function addPlugin(
-  name: string,
-  pluginList: RollupPluginList,
-  plugins: CombineOptions['plugins']
-) {
-  const resolvedPlugins = plugins.map((plugin) =>
-    resolvePlugin(plugin, 'rollup')
-  )
-
-  const index = pluginList.findIndex((plugin) => plugin && plugin.name === name)
-  pluginList.splice(index + 1, 0, ...resolvedPlugins)
-}
+import type { Factory, FactoryOutput, RollupPlugin } from './types'
 
 export const getRollupPlugin = <UserOptions>(
-  factory: Factory<UserOptions>,
-  vite = false
-): FactoryOutput<UserOptions, RollupPlugin> => {
+  factory: Factory<UserOptions>
+): FactoryOutput<UserOptions, RollupPlugin[]> => {
   return (userOptions?: UserOptions) => {
-    const { name, enforce, plugins } = factory(userOptions!)
-
-    const plugin: RollupPlugin = {
-      name,
-      options(options) {
-        options.plugins ||= []
-        addPlugin(name, options.plugins, plugins)
-      },
-    }
-
-    if (vite) {
-      ;(plugin as VitePlugin).enforce = enforce
-    }
-
-    return plugin
+    const { plugins } = factory(userOptions!)
+    return plugins.map((plugin) => resolvePlugin(plugin, 'rollup'))
   }
 }
