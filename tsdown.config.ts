83c7b90ab976151d389fc403fa24b551ee4621b5
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-01-30 01:20:21 +0800

    refactor: switch to tsdown

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'tsup'
+import { defineConfig } from 'tsdown'
 
 export default defineConfig({
   entry: ['./src/*.ts', '!./src/types.ts'],
   format: ['cjs', 'esm'],
   target: 'node16.14',
-  splitting: true,
   clean: true,
   dts: true,
+  publint: true,
 })
