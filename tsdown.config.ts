8963af260249696d1028fd713e1009111de1a771
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-05-09 02:32:58 +0800

    feat!: drop node 18 & cjs build

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -2,9 +2,5 @@ import { defineConfig } from 'tsdown'
 
 export default defineConfig({
   entry: ['./src/*.ts', '!./src/types.ts'],
-  format: ['cjs', 'esm'],
-  target: 'node16.14',
-  clean: true,
-  dts: true,
   publint: true,
 })
